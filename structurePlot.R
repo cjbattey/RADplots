02f385e40df696f3e1378bf56469b12107a3d72d
CJ Battey <cjbattey@gmail.com>
2015-04-07 18:02:31 -0700

    fixed issue with 100+ samples

--- structurePlot.R
+++ structurePlot.R
@@ -13,56 +13,50 @@
 ##If color.matching=TRUE, user-specified colors are assigned by population number - first color in the vector for pop 1, second for pop 2, etc.
 ##
 ##To make plots that look like the standard STRUCTURE plots from the GUI, set spacing=0 and outline='black'
+#c("gold","forestgreen","magenta3","orangered","cornflowerblue","orange","sienna","dodgerblue4")
+#c(colors=brewer.pal(12,"Set3"))
 
 structurePlot <- function(strOutput,pop.order=FALSE,pop.info=TRUE,colors=c("gold","forestgreen","magenta3","orangered","cornflowerblue","orange","sienna","dodgerblue4"),color.matching=FALSE,spacing=0.2,outline=NA) {
-  require(plyr);require(reshape)
+  require(plyr);require(reshape);require(RColorBrewer)
   infile <- readLines(strOutput,warn=FALSE)
   a <- grep("Inferred ancestry of individuals:",infile)+2
   b <- grep("Estimated Ln Prob of Data",infile)
   c <- grep("Estimated Allele Frequencies in each cluster",infile)-3
   k <- as.numeric(substr(infile[grep("populations assumed",infile)],4,5))
   lnml <- infile[b]
-  df <- infile[(a):(c)]
-  df <- strsplit(df," +")
-  df <- do.call(rbind.data.frame, df)
+  df <- read.table(strOutput,skip=a-1,nrow=(c-a+1))
+  df <- df[2:ncol(df)]
   if (pop.info=="TRUE"){
-    df <- data.frame(df[3],df[5],df[7:(6+k)])
-    colnames(df) <- c("sample","pop",1:k)
+    colnames(df) <- c("sample","percent.missing","pop","X",1:k)
     n.pops <- max(as.numeric(as.character(df$pop)))
   if (color.matching=="TRUE"){
     palette <- c(rep("NA",k))
     for(i in 1:n.pops){
-      d <- subset(df,pop==i)[3:(2+k)]
-      for(j in 1:ncol(d)) {
-        d[,j] <- as.numeric(as.character(d[,j]))
-      }
-      e <- colMeans(d)
+      d <- subset(df,pop==i)
+      e <- colMeans(df[5:ncol(df)])
       f <- as.numeric(names(e[which(e==max(e))]))
       if (palette[f] == "NA"){
         palette[f] <- colors[i]
         }
       }
-    if (n.pops < k) {
-      for (i in 1:length(colors)){
-          palette[which(palette == "NA")] <- colors[which(colors%in%palette == FALSE)]
-        }
-      }
+    palette[which(palette == "NA")] <- colors[which(colors%in%palette == FALSE)]
     colors <- palette
    }
   if (pop.order=="TRUE"){
-    df <- arrange(df,df$pop)
-    barplot(t(as.matrix(df[3:(2+k)])),axes=FALSE,col=colors,border=outline,names=df$sample,cex.names=0.75,las=2,main=lnml,cex.main=0.75,font.main=1,space=spacing,xpd=FALSE)
+    df <- arrange(df,pop)
+    a <- df[5:ncol(df)]
+    barplot(t(a),axes=FALSE,col=colors,border=outline,names=df$sample,cex.names=0.75,las=2,main=lnml,cex.main=0.75,font.main=1,space=spacing,xpd=FALSE)
   }
-    barplot(t(as.matrix(df[3:(2+k)])),axes=FALSE,col=colors,border=outline,names=df$sample,cex.names=0.75,las=2,main=lnml,cex.main=0.75,font.main=1,space=spacing,xpd=FALSE)
+    a <- df[5:ncol(df)]
+    barplot(t(a),axes=FALSE,col=colors,border=outline,names=df$sample,cex.names=0.75,las=2,main=lnml,cex.main=0.75,font.main=1,space=spacing,xpd=FALSE)
   }
   else {
-    df <- data.frame(df[3],df[6:(5+k)])
-    colnames(df) <- c("sample",1:k)
+    colnames(df) <- c("sample","percent.missing","X",1:k)
+    a <- df[4:ncol(df)]
+    barplot(t(a),axes=FALSE,col=colors,border=outline,names=df$sample,cex.names=0.75,las=2,main=lnml,cex.main=0.75,font.main=1,space=spacing,xpd=FALSE)
     n.pops <- k
     names <- df$sample
-    sample <- "sample"
-    df <- df[,!(names(df)) %in% sample]
-    barplot(t(as.matrix(df)),axes=FALSE,col=colors,border=outline,names=names,cex.names=0.75,las=2,main=lnml,cex.main=0.75,font.main=1,space=spacing,xpd=FALSE)
+    barplot(t(a),axes=FALSE,col=colors,border=outline,names=names,cex.names=0.75,las=2,main=lnml,cex.main=0.75,font.main=1,space=spacing,xpd=FALSE)
   }
 } 
 
