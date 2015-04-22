2bb06d81dcfb03e1525d1404c62090c12e63915a
CJ Battey <cjbattey@gmail.com>
2015-04-21 17:18:03 -0700

    updated for random # seed and SlagerThreader use

--- multiStructure.R
+++ multiStructure.R
@@ -1,26 +1,19 @@
-## Obsolete vis-a-viz slagerThreader.py - probably faster to copy/paste a shell script together.
+##Quick script to write a shell file for batch runs of structure via slagerthreader.py
+##adjust runs and k range as needed.
 
-#Function to write shell scripts for executing parallel runs of the program STRUCTURE. 
-##File paths can be complete or from the working diretory you intend to use. 
-##structureCommand is the command to run structure - either the path to the 
-#structure.bin file (it's in the .app - right click and go to show contents on a Mac to find it), or just "structure" if you have it in /usr/bin
-##Output will be n shell scripts saved to your working directory, where n is the number of threads.
-##
-##To run these, just open a separate terminal window for each thread and run one shell script per terminal window. 
-##Note that you'll also need to run <chmod +x shellscript.sh> to make the scripts executable before running them. 
-##This is a pain in the butt, but I don't know a way around it. 
+runs <- c(1:10)
+krange <- c(2:8)
 
-multiStructure <- function(outfile,structureCommand,mainparams,threads,nruns){ 
-  shell <- c()
-  run <- 1
-  for (i in 1:threads) {
-    for (j in 1:(nruns/threads)){
-      command <- paste(structureCommand," -m ",mainparams," -o ",paste(outfile,"_run",run,sep="")," > ",paste(outfile,"_run",run,".log",sep=""),"\n",sep="")
-      shell <- append(shell,command)
-      run <- run+1
+shell <- c()
+    for (i in krange){
+      for(j in runs){
+        randseed <- as.integer(runif(1,min=1,max=1000000))
+        command <- paste("structure"," -m ",paste("./mainparams","_k",i,sep="")," -D ",
+                         paste(randseed)," -o ",paste("./output/k",i,"_run",j,sep="")," > ",
+                         paste("./output/k",i,"_run",j,".log",sep=""),"\n",sep="")
+        shell <- append(shell,command)
+      }
     }
-    write(shell,paste("thread_",i,".sh",sep=""))
-    shell <- c()
-  }
-}
 
+write(shell,"structure.sh")
+    
